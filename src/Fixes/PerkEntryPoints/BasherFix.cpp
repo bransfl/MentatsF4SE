5c8a207d447773065533c9c5a4d99e0d8cc4b84f
bp42s <140995694+bp42s@users.noreply.github.com>
2025-02-20 02:51:04 -0500

    Updated basher to fix hpp file

--- src/Fixes/PerkEntryPoints/BasherFix.cpp
+++ src/Fixes/PerkEntryPoints/BasherFix.cpp
@@ -1,17 +1,22 @@
-#include "Internal/Config/Config.hpp"
 #include "Internal/Fixes/PerkEntryPoints/BasherFix.hpp"
+#include "Internal/Config/Config.hpp"
 
-// fixes entrypoint kModOutgoingLimbBashDamage so it actually works
+// fixes entrypoint ModOutgoingLimbBashDamage so it actually works
+// note - this will likely require RE, and wont be made until RE is done.
 namespace Internal::Fixes::PerkEntryPoints::BasherFix
 {
-	void Install() noexcept {
+	void Install() noexcept
+	{
 		logger::info("Fix installing: BasherFix.");
 
 		if (!Config::bBasherFix.GetValue()) {
 			logger::info("Fix aborted: BasherFix. Reason: Fix was disabled in ini file.");
 			return;
 		}
-		
+
 		logger::info("Fix installed: BasherFix.");
 	}
+
+	// RE::BGSEntryPoint::ENTRY_POINT::kModBashingDamage = 0x1A,
+
 }

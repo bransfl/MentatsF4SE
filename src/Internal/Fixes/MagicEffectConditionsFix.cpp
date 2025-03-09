cfe1c1908f97543bf1a5e888a8ebd01241420c43
bp42s <140995694+bp42s@users.noreply.github.com>
2025-03-09 12:06:17 -0400

    Hopefully fixed MagicEffectConditionFix's radiation bug

--- src/Internal/Fixes/MagicEffectConditionsFix.cpp
+++ src/Internal/Fixes/MagicEffectConditionsFix.cpp
@@ -4,8 +4,13 @@
 namespace Internal::Fixes
 {
 	typedef void(EvaluateConditionsSig)(RE::ActiveEffect*, float, bool);
-	REL::Relocation<EvaluateConditionsSig> OriginalFunction_EvaluateConditions_OG;
-	REL::Relocation<EvaluateConditionsSig> OriginalFunction_EvaluateConditions_NG;
+	REL::Relocation<EvaluateConditionsSig> OriginalFunction_EvaluateConditions;
+
+	// thank you bingle my beloved
+	REL::Relocation<RE::Setting*> fActiveEffectConditionUpdateInterval{ REL::ID(553783) };
+	REL::Relocation<uintptr_t> ptr_EvaluateConditions{ REL::ID(1228998) };
+	REL::Relocation<uint32_t*> unkCheck{ REL::ID(607340) };
+	REL::Relocation<float*> fcheckConditionsRate{ REL::ID(1288592) };
 
 	void MagicEffectConditionsFix::Install() noexcept
 	{
@@ -32,7 +37,7 @@ namespace Internal::Fixes
 		else {
 			// OG Patch
 			REL::Relocation<uintptr_t> ptr_EvaluateConditions_OG{ REL::ID(1228998) };
-			OriginalFunction_EvaluateConditions_OG = trampoline.write_branch<5>(ptr_EvaluateConditions_OG.address(), &Hook_EvaluateConditions);
+			OriginalFunction_EvaluateConditions = trampoline.write_branch<5>(ptr_EvaluateConditions_OG.address(), &Hook_EvaluateConditions);
 		}
 
 		logger::info("Fix installed: MagicEffectConditionsFix."sv);
@@ -49,58 +54,67 @@ namespace Internal::Fixes
 		}
 	}
 
+	// note - EffectSetting has a lot of magiceffect data
+	// kNotAvailable = no conditions on the record
+
 	void MagicEffectConditionsFix::Hook_EvaluateConditions(RE::ActiveEffect* activeEffect, float elapsedTimeDelta, bool forceUpdate)
 	{
+		logger::info("activeEffect was: GetFormID()={:08X}, EditorId={}"sv, activeEffect->spell->GetFormID(), activeEffect->spell->GetFormEditorID());
+
 		if (activeEffect->conditionStatus == RE::ActiveEffect::ConditionStatus::kNotAvailable) {
-			// logger::debug("MagicEffectConditions -> activeEffect's ConditionStatus was kNotAvailable. Return."sv);
+			logger::info("MagicEffectConditions -> activeEffect's ConditionStatus was kNotAvailable. Return."sv);
 			return;
 		}
 
-		// we check this to fix the addiction bug in the original mod
 		if (activeEffect->spell->GetCastingType() == RE::MagicSystem::CastingType::kFireAndForget) {
-			RE::TESForm* form = RE::TESForm::GetFormByID(activeEffect->spell->GetFormID());
-			if (!form)
-				return;
+			logger::info("activeEffect was FireAndForget. GetFormID()={:08X}, EditorId={}"sv, activeEffect->spell->GetFormID(), activeEffect->spell->GetFormEditorID());
 
-			RE::AlchemyItem* potion = form->As<RE::AlchemyItem>();
-			if (!potion)
-				return;
-
-			// logger::debug("activeEffect was FaF potion. GetFormID()={:08X}, EditorId={}"), potion->GetFormID(), potion->GetFormEditorID());
-			if (potion->data.addictionChance > 0.0 || potion->data.addictionItem != nullptr) {
-				// logger::debug("activeEffect potion WAS addictive. addiction: {}"), potion->data.addictionName);
-				return;
+			RE::AlchemyItem* potion = RE::TESForm::GetFormByID(activeEffect->spell->GetFormID())->As<RE::AlchemyItem>();
+			if (potion) {
+				logger::info("activeEffect was FireAndForget potion. GetFormID()={:08X}, EditorId={}"sv, potion->GetFormID(), potion->GetFormEditorID());
+				if (potion->data.addictionChance > 0.0 || potion->data.addictionItem != nullptr) {
+					logger::info("activeEffect potion WAS addictive. addiction: {}"sv, potion->data.addictionName);
+					return;
+				}
+			}
+			else {
+				logger::info("activeEffect potion was null. GetFormID()={:08X}, EditorId={}"sv, activeEffect->spell->GetFormID(), activeEffect->spell->GetFormEditorID());
 			}
 		}
 
 		// if (all effect conditions are true OR this is a displacement spell) AND (the target is valid AND the target's object is valid)
 		if ((activeEffect->flags.all(RE::ActiveEffect::Flags::kHasConditions) || activeEffect->displacementSpell) && activeEffect->target && activeEffect->target->GetTargetStatsObject()) {
 
-			// auxillary timer
+			// store the auxillary timer on the unused member variable pad94
 			auto& conditionUpdateTime = reinterpret_cast<float&>(activeEffect->pad94);
 
 			if (!forceUpdate) {
 				if (activeEffect->elapsedSeconds <= 0.0F) {
+					// set the auxillary timer to the amt of the effect has been active
 					reinterpret_cast<float&>(activeEffect->pad94) = elapsedTimeDelta;
+					logger::info("MagicEffectConditions -> activeEffect's pad94 was set to elapsedTimeDelta"sv);
 					return;
 				}
 
 				if (conditionUpdateTime > 0.0F && conditionUpdateTime < ActiveEffectConditionUpdateInterval()) {
 					reinterpret_cast<float&>(activeEffect->pad94) += elapsedTimeDelta;
+					logger::info("MagicEffectConditions -> activeEffect's pad94 had elapsedTimeDelta added."sv);
 					return;
 				}
 			}
 
-			RE::TESObjectREFR* caster = activeEffect->caster.get().get(); // note - this might need to be target?
-			if (activeEffect->effect->conditions.IsTrue(activeEffect->target->GetTargetStatsObject(), caster) && !activeEffect->CheckDisplacementSpellOnTarget()) {
+			if (activeEffect->effect->conditions.IsTrue(activeEffect->target->GetTargetStatsObject(), activeEffect->caster.get().get()) && !activeEffect->CheckDisplacementSpellOnTarget()) {
 				activeEffect->conditionStatus = RE::ActiveEffect::ConditionStatus::kTrue;
+				logger::info("MagicEffectConditions -> activeEffect's ConditionStatus was set to kTrue for GetFormID()={:08X}, EditorId={}"sv, activeEffect->spell->GetFormID(), activeEffect->spell->GetFormEditorID());
 			}
 			else {
 				activeEffect->conditionStatus = RE::ActiveEffect::ConditionStatus::kFalse;
+				logger::info("MagicEffectConditions -> activeEffect ConditionStatus was set to kFalse for GetFormID()={:08X}, EditorId={}"sv, activeEffect->spell->GetFormID(), activeEffect->spell->GetFormEditorID());
 			}
 		}
 		else {
 			activeEffect->conditionStatus = RE::ActiveEffect::ConditionStatus::kNotAvailable;
+			logger::info("MagicEffectConditions -> activeEffect's ConditionStatus was set to kNotAvailable for GetFormID()={:08X}, EditorId={}"sv, activeEffect->spell->GetFormID(), activeEffect->spell->GetFormEditorID());
 		}
 	}
 }
